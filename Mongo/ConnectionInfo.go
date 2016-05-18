b52c971a305f3c793686fef1a2f46aa17e6138d0
Chad England <chadit@gmail.com>
2016-05-18 16:00:47 -0400

    add more common functionality to repo

--- Mongo/ConnectionInfo.go
+++ Mongo/ConnectionInfo.go
@@ -23,8 +23,6 @@ type Tenant struct {
 	ExternalID string `json:"ExternalId" bson:"ExternalId,omitempty"`
 	// (Migration/Sync only)ExternalNumber is used to sync with external systems (Not used for anything internally)
 	ExternalNumber string `json:"ExternalNumber" bson:"ExternalNumber,omitempty"`
-	// (Migration/Sync only)used to validate systems and keep them in sync (Not used for anything internally)
-	HashCode string `json:"HashCode"  bson:"HashCode,omitempty"`
 	// TenantID pertains to the tenant the document belongs to.  this is not serialized out
 	TenantID string `json:"-" bson:"TenantId"`
 
@@ -66,4 +64,48 @@ type TenantConnectionInfo struct {
 // InitBaseTenantConnectionInfo sets the defaults for a new userlogin
 func (p *TenantConnectionInfo) InitBaseTenantConnectionInfo() {
 	p.ConnectionString = Shares.MongoConnection
+}
+
+// Node model used to hold tenant connection information
+type Node struct {
+	// (ReadOnly) Id of the document, created by system utilizing Mongo Bson Id
+	ID string `json:"Id"  bson:"_id" binding:"required"`
+	// (ReadOnly) Date the document was created (UTC)
+	DateCreated time.Time `json:"DateCreated" bson:"DateCreated" binding:"required"`
+	// (ReadOnly) Date the document was modified (UTC)
+	DateModified time.Time `json:"DateModified" bson:"DateModified"`
+	// (ReadOnly) What user modified the document
+	UserModified string `json:"UserModified" bson:"UserModified,omitempty"`
+	// (ReadOnly) Has the record been deleted
+	IsDeleted bool `json:"IsDeleted" bson:"IsDeleted,omitempty"`
+	// (Migration/Sync only)ExternalId is used to sync with external systems (Not used for anything internally)
+	ExternalID string `json:"ExternalId" bson:"ExternalId,omitempty"`
+	// (Migration/Sync only)ExternalNumber is used to sync with external systems (Not used for anything internally)
+	ExternalNumber string `json:"ExternalNumber" bson:"ExternalNumber,omitempty"`
+	// TenantID pertains to the tenant the document belongs to.  this is not serialized out
+	TenantID   string `json:"-" bson:"TenantId"`
+	Name       string `json:"Name" bson:"Name" binding:"required"`
+	Connection string `json:"Connection"  bson:"Connection" binding:"required"`
+}
+
+// InitNode sets the defaults for a new node
+func (p *Node) InitNode() {
+	eventTime := time.Now().UTC()
+	p.ID = bson.NewObjectId().Hex()
+	p.DateCreated = eventTime
+	p.DateModified = eventTime
+	p.IsDeleted = false
+}
+
+// GetBaseConnectionInformation will attempt to locate the tenant by provided informaiton
+func GetBaseConnectionInformation(collectionName string) (*TenantConnectionInfo, error) {
+	item := new(Tenant)
+	item.ID = "global"
+	item.Name = "global"
+	baseConnectionInfo := new(TenantConnectionInfo)
+	baseConnectionInfo.InitBaseTenantConnectionInfo()
+	baseConnectionInfo.Tenant = *item
+	baseConnectionInfo.CollectionName = collectionName
+
+	return baseConnectionInfo, nil
 }
